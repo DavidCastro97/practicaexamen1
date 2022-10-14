f5fa07f57bbc5327bc35caeefceed36c350e73f4
abraham1798 <abrahamc.a@hotmail.com>
2022-10-14 01:46:49 -0600

    SE COMENTA EL CODIGO

--- mavenproject2/src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
@@ -20,7 +20,7 @@ public class Mavenproject2 {
     public static void main(String[] args) {
         //System.out.println("Hello World!");
        
-       
+       //LLAMAR LA CLASE
                 
 
                     
